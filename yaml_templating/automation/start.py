8a7a12975b002060119b13a4f4543017d6ce03cb
Haynes <martinh>
2023-06-09 13:32:24 +0200

    https://jcdan3.medium.com/how-to-run-python-scripts-in-your-github-actions-caccac969b34 14

---
+++ yaml_templating/automation/start.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python
+
+import sys
+import os
+import json
+
+from modules.do_something import task1 as nesmysl
+
+def cheese(somevar, someother):
+    for k, v in someother.iteritems():
+        print(f"Key : {k}")
+
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+if __name__== "__main__":
+    print("Couldn't open the other repo !")
